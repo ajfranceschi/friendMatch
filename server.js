af84b5728d10bf5a5a7a0d6374b40a1e12e007bb
Antonio Franceschi <antonio@Antonios-MacBook-Pro.local>
2019-09-22 03:04:14 -0400

    server.js: added survey route

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/app/public/home.html'))
+  res.sendFile(path.join(__dirname, '/app/public/home.html'));
 });
 
+app.get('/survey', (req, res) => {
+  res.sendFile(path.join(__dirname, '/app/public/survey.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server up and listening on port ${PORT}`)
