bcdabf54d63797c8d064366a9b602cb5b112074f
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-10-11 15:16:51 -0400

    added surveyed user's image to match modal. added validation.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,12 @@
 const friends = [
   {
     name: "Ahmed",
-    photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    photo: "https://images.unsplash.com/photo-1541253058872-dba3d6bb6d97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
   {
     name: "Jacob Deming",
-    photo:
-      "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
     scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
   },
   {
@@ -24,14 +22,12 @@ const friends = [
   },
   {
     name: "Lou Ritter",
-    photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+    photo: "https://images.unsplash.com/photo-1564595063998-fc31c376d0df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
     scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
   },
   {
     name: "Jordan Biason",
-    photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+    photo: "https://images.unsplash.com/photo-1554457945-229b8867b30d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1867&q=80",
     scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
   },
   {
--- app/public/js/survey.js
+++ app/public/js/survey.js
@@ -1,54 +1,83 @@
 $("#surveySubmit").on("click", event => {
   event.preventDefault();
+  const matchImage = $("#matchImage");
+  const userImage = $("#userImage");
+  const userBody = $("#user-body");
+  const matchBody = $("#match-body");
+  const matchModal = $("#match-modal");
+  const matchModalTitle = $("#modalMatchTitle");
+  const modalSubtitle = $("#modalSubtitle");
 
   const name = $("#name")
     .val()
     .trim();
-  const imageUrl = $("#pictureUrl")
+  const userImageUrl = $("#pictureUrl")
     .val()
     .trim();
-  const question1 = Number($("#question1").val());
-  const question2 = Number($("#question2").val());
-  const question3 = Number($("#question3").val());
-  const question4 = Number($("#question4").val());
-  const question5 = Number($("#question5").val());
-  const question6 = Number($("#question6").val());
-  const question7 = Number($("#question7").val());
-  const question8 = Number($("#question8").val());
-  const question9 = Number($("#question9").val());
-  const question10 = Number($("#question10").val());
 
-  const userAnswers = {
-    name,
-    imageUrl,
-    question1,
-    question2,
-    question3,
-    question4,
-    question5,
-    question6,
-    question7,
-    question8,
-    question9,
-    question10
-  };
+  //validate name and picture
+  if (name === "" || userImageUrl === "") {
+    matchImage.hide();
+    userImage.hide();
+    userBody.hide();
+    matchBody.hide();
+    matchModal.modal("show");
+    matchModalTitle.text("Missing Required Fields");
+    modalSubtitle.html(
+      "Make sure to enter both <b>Name</b> and <b>Link</b> to Photo."
+    );
+  } else {
+    const question1 = Number($("#question1").val());
+    const question2 = Number($("#question2").val());
+    const question3 = Number($("#question3").val());
+    const question4 = Number($("#question4").val());
+    const question5 = Number($("#question5").val());
+    const question6 = Number($("#question6").val());
+    const question7 = Number($("#question7").val());
+    const question8 = Number($("#question8").val());
+    const question9 = Number($("#question9").val());
+    const question10 = Number($("#question10").val());
 
-  fetch("/api/friends", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(userAnswers)
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      $('#match-modal').modal('show');
-      $('#matchImage').attr('src', data.photo);
-      $('#matchImage').height(200);
-      $("#modalMatchTitle").text(`You Matched: ${data.name}`);
+    const userAnswers = {
+      name,
+      imageUrl: userImageUrl,
+      question1,
+      question2,
+      question3,
+      question4,
+      question5,
+      question6,
+      question7,
+      question8,
+      question9,
+      question10
+    };
+
+    fetch("/api/friends", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(userAnswers)
     })
-    .catch(error => {
-      console.log(error);
-    });
+      .then(response => response.json())
+      .then(data => {
+        matchImage.show();
+        userImage.show();
+        userBody.show();
+        matchBody.show();
+        matchModal.modal("show");
+        matchModalTitle.text(`Match Found`);
+        modalSubtitle.text("You matched...");
+        userBody.text(`${name}`);
+        userImage.attr('src',userImageUrl);
+        matchBody.text(`${data.name}`)
+        matchImage.attr("src", data.photo);
+        $("img").height(200);
+        $("form").trigger("reset");
+      })
+      .catch(error => {
+        console.log(error);
+      });
 
-  $("form").trigger("reset");
+
+  }
 });
